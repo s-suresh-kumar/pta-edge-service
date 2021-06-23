674cd118b23ac8e2346a68693e9de7a22c2cea0b
Suresh Kumar <suresh.uncbc.coding@gmail.com>
2021-06-22 23:52:03 -0400

    Initial Commit

---
+++ src/main/java/com/cognizant/ptaedgeservice/PtaEdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtaEdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtaEdgeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/ptaedgeservice/PtaEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.ptaedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaEdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
